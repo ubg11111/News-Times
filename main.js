d084f3822c486ad05339734c761fad5d74889f37
bangdal <ubg11111@naver.com>
2022-08-01 17:13:22 +0900

    페이지네이션 검색

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // news 전역변수를 빈 배열로 선언
 let news = [];
+let page = 1;
+let total_pages = 0;
 
 // url을 전역변수로 선언하여 각 함수에 적용하게끔 한다.
 let url;
@@ -20,25 +22,51 @@ menusButton.forEach((menu) => menu.addEventListener("click", (event) => getNewsB
 // 중복되는 코드들을 함수로 묶어준다.
 const getNews = async() =>{
     
-    // new Headers() 기능을 사용하여 key,value값을 넣어준다. (토큰)
-    let header = new Headers(
-    {'x-api-key': 'strtLJ_t-qztFwGhxuNS7HAgun0bSzbb4Q56ggHt4BU'}
-    );
+    try{
+        // new Headers() 기능을 사용하여 key,value값을 넣어준다. (토큰)
+        let header = new Headers(
+            {'x-api-key': 'strtLJ_t-qztFwGhxuNS7HAgun0bSzbb4Q56ggHt4BU'}
+        );
 
-    // await을 통해 fetch를 잠깐 대기 시킨다.
-    let response = await fetch(url, {headers : header}); // ajax,http,fetch 를 사용할 수 있음
 
-    // response 데이터를 json타입으로 변환한다.
-    let data = await response.json();
-    
-    console.log(data);
+        // url에 쿼리를 더해준다.
+        url.searchParams.set('page', page); // &page= (page전역변수)
 
-    // 빈 배열에 data의 담겨진 articles 내용을 넣어준다.
-    news = data.articles;
+        console.log("url은?",url);
 
-    console.log(news);
+        // await을 통해 fetch를 잠깐 대기 시킨다.
+        let response = await fetch(url, {headers : header}); // ajax,http,fetch 를 사용할 수 있음
 
-    render();
+        // response 데이터를 json타입으로 변환한다.
+        let data = await response.json();
+        
+        // response의 status가 200이면 ok
+        if(response.status === 200){
+            
+            // total_hits 검색된 조회수가 0인 경우는 유저에게 아래와같은 메시지오류를 출력하여 보여주게 됨.
+            if(data.total_hits == 0){
+                throw new Error("검색된 결과값이 없습니다.");
+            }
+            
+            news = data.articles;
+            total_pages = data.total_pages;
+            page = data.page;
+
+            // 빈 배열에 data의 담겨진 articles 내용을 넣어준다.
+
+            console.log(news);
+            console.log("받는 데이터 확인하기",data)
+            render();
+            pageNation();
+        }else{
+            throw new Error(data.message)
+        }
+     
+    }catch(error){
+        console.log("잡힌 에러", error.message);
+        errorRender(error.message);
+    }
+   
 }
 
 
@@ -82,8 +110,6 @@ const getNewsByKeyword = async() => {
 }
 
 
-
-
 const render = () => {
     let newsHTML = '';
 
@@ -113,6 +139,96 @@ const render = () => {
     // 뉴스 내용이 들어갈 섹션의 아이디를 가져와서 뉴스의 내용을 넣는다.
     document.querySelector("#news-board").innerHTML = newsHTML;
 };
+
+
+const errorRender = (message) =>{
+
+    let errorHTML = `<div class="alert alert-danger text-center" role="alert" >
+     ${message}
+    </div>`
+    document.querySelector("#news-board").innerHTML = errorHTML;
+}
+
+const pageNation = () => {
+
+    let pageNationHTML = ``;
+
+    // total_page 
+    // page
+    // page_group
+    let page_group = Math.ceil(page / 5);
+    // last page
+    let last =  page_group * 5;
+        if(last > total_pages){
+            // 마지막에 그룹이 5개 이하라면...
+            last = total_pages;
+        }
+
+    // first page
+    let first = last - 4 <= 0 ? 1 : last - 4; // 첫그룹이 5이하이면
+    
+    // total page 3일 경우 3개의 페이지만 프린트 하는법 last, first
+    // << >> 버튼 만들어주기..
+
+
+    if(first >= 6){
+        pageNationHTML += `<li class="page-item">
+        <a class="page-link" href="#" aria-label="Previous" onclick="moveToPage(1)">
+          <span aria-hidden="true">&lt&lt;</span>
+        </a>
+        </li>
+        <li class="page-item">
+        <a class="page-link" href="#" aria-label="Previous" onclick="moveToPage(${page - 1})">
+          <span aria-hidden="true">&lt;</span>
+        </a>
+        </li>
+        `
+    }
+ 
+
+    
+    // first page ~ last page 페이지 프린트
+    for(let i=first; i<=last; i++){
+        
+        // pageNationHTML 값에 += 반복문을 통해 li값을 반복하여 넣어준다.
+        // onclick 기능에 moveToPage() 함수를 넣고 기능작업을 해준다. 인자로 i(first)값을 넘겨준다.
+        // 삼항연사자 활용 page의 값이 현재 페이지 i랑 같다면 ? "active":"" 같으면 표시를 해준다.
+        pageNationHTML +=  `<li class="page-item ${page == i ? "active":""}"><a class="page-link" href="#" onclick="moveToPage(${i})">${i}</a></li>`
+
+    }
+
+    if(last < total_pages){
+        pageNationHTML +=   `<li class="page-item">
+        <a class="page-link" href="#" aria-label="Next" onclick="moveToPage(${page + 1})">
+          <span aria-hidden="true">&gt;</span>
+        </a>
+        </li>
+        <li class="page-item">
+        <a class="page-link" href="#" aria-label="Previous" onclick="moveToPage(${total_pages})">
+          <span aria-hidden="true">&gt&gt;</span>
+        </a>
+        </li>
+        `
+    }
+  
+   
+
+
+    document.querySelector(".pagination").innerHTML = pageNationHTML;
+}
+
+const moveToPage = (pageNum) => {
+    // 1. 이동하고 싶은 페이지를 확인해야함.
+    
+
+    page = pageNum;
+
+    // 2. 이동하고 싶은 페이지를 가지고 api를 다시 호출해주자.
+
+    getNews();
+
+}
+
 
 searchButton.addEventListener("click", getNewsByKeyword);
 getLatestNews();
